1911b118283f62e4ab803d79a4aec54abfb3da3b
lalgutwa-essentials <81512983+lalgutwa-essentials@users.noreply.github.com>
2021-03-30 10:11:56 +0530

    Add files via upload
    update version 1.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,3 @@
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
 }
-
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 23.358444, lng: 85.239454 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-}
-  
